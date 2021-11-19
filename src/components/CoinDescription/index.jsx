1d94baed55c7bbf774f3366402c9def03164d7fc
Zahid Ronty <230759@usn.no>
2021-11-19 01:27:27 +0100

    First

---
+++ src/components/CoinDescription/index.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default function CoinDescription({ description }) {
+  const styles = {
+    container: "shadow px-12 py-6 bg-white mx-12 rounded-lg text-lg font-semibold text-justify"
+  }
+  return (
+    <div className={styles.container} >
+      <p>{description}</p>
+    </div>
+  );
+}
+
+CoinDescription.propTypes = {
+  description: PropTypes.string.isRequired
+}
+
